2e65b3728f7f16da5f7b6f5455793c825ee3d310
Глеб Галеев <wiandop@MacBook-Pro-Gleb.local>
2024-05-11 16:49:21 +0300

    Updated code comments

--- parse2csv.py
+++ parse2csv.py
@@ -1,10 +1,29 @@
 import pandas as pd
 import glob
 
+'''
+data2csv - function for converting a data array into csv format
+
+Arguments: 
+    data - an array with site data like: src, link, content
+    src - a string containing information about the site from which the articles were parsed
+
+Libraries:
+    pandas: pip install pandas
+'''
 def data2csv(data, src):
     df = pd.DataFrame(data)
     df.to_csv(src + '.csv', sep='\t')
 
+    return
+
+'''
+merge_csv - function for merging csv files
+
+Libraries:
+    pandas: pip install pandas
+    glob
+'''
 def merge_csv():
     csvFiles = glob.glob('*.{}'.format('csv'))
     mergedCSV = pd.DataFrame()
@@ -14,3 +33,5 @@ def merge_csv():
         mergedCSV = mergedCSV.append(df, ignore_index=True)
 
     df.to_csv('articles.csv', sep='\t')
+
+    return
